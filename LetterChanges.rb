2ac194242c20d240d80e36e5e6a365a9e8c5f891
cjpasch <cjpasch91@gmail.com>
2015-06-11 19:42:43 -0400

    Create LetterChanges.rb

---
+++ LetterChanges.rb
@@ -0,0 +1,36 @@
+# LetterChanges_Ruby
+# Coderbytes Problem: Letter Changes w/ Ruby
+
+def LetterChanges(str)
+
+split = str.split(//)
+alphabet = ('a'..'z').to_a
+
+alphabet.each do |letter|
+	print "#{letter} "
+end
+
+puts
+
+i = 0
+letter = 0
+while i < split.length
+	if alphabet[letter] == split[i]
+	puts "true"
+	i += 1
+	letter = 0
+	else alphabet[letter] != split[i]
+	puts "shit!"
+	letter += 1
+	end
+end
+
+end
+
+LetterChanges("argument goes here")
+# results are
+#=> prints a-z perfectly
+#=> puts "true" 13 times, once for each letter of "argument".
+#=> BUT letter does not equal " ", and goes into an infinite loop print "shit" forever.
+
+
